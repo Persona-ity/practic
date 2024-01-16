6ab90565bd2f53f1ef891562e10425e62eca994a
Persona-ity <studyscience1955@gmail.com>
2024-01-16 09:49:44 +0700

    First version

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let product = "Socks";
+
+let app = new Vue({
+    el: '#app',
+    data: {
+    product: "Socks"
+    }
+})
+
+let app2 = new Vue({
+    el: '#app2',
+    data: {
+        description: "A pair of warm, fuzzy socks"
+    }
+})
