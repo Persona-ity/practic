92024f1773751cc8f29e2929c8a288d66b3b5520
Persona-ity <studyscience1955@gmail.com>
2024-01-16 13:49:58 +0700

    недоделанное

--- js/main.js
+++ js/main.js
@@ -4,10 +4,14 @@ let app = new Vue({
     el: '#app',
     data: {
     product: "Socks",
-    image: "./assets/vmSocks-blue-onWhite.jpg",
+    brand: 'Vue Mastery',
+    selectedVariant: 0,
+    
     altText: "A pair of socks",
     link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-    inStock: true,
+    inStock(){
+        return this.variants[this.selectedVariant].variantQuantity
+    },
     inventory: 0,
     onSale: false,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
@@ -28,17 +32,25 @@ let app = new Vue({
         variantId: 2234,
         variantColor: 'green',
         variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10
         },
         {
         variantId: 2235,
         variantColor: 'blue',
         variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0
         }
         ],
         updateProduct(variantImage) {
             this.image = variantImage
         },
-        
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
     },
     methods: {
         addToCart() {
@@ -47,7 +59,14 @@ let app = new Vue({
         MinusToCart() {
             this.cart -= 1
         }
-    }
+    },
+    computed: {
+        title() {
+        return this.brand + ' ' + this.product;
+        }
+    },
+    
+    
 })
 
 let app2 = new Vue({
